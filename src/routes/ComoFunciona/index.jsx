1aab6f3ac240ace668110dd45a25a5e50d8c8082
João Gabriel Fröhlich <gabriel@gabrielfrohlich.dev>
2025-05-19 19:30:21 -0300

    Adição de páginas

---
+++ src/routes/ComoFunciona/index.jsx
@@ -0,0 +1,11 @@
+function ComoFunciona() {
+  return (
+    <div className="container">
+      <h1>Como Funciona</h1>
+      <p>Esta página explica como funciona o sistema.</p>
+      <p>Em breve, mais informações serão adicionadas.</p>
+    </div>
+  );
+}
+
+export default ComoFunciona;
---
+++ src/routes/Funcionalidades/index.jsx
@@ -0,0 +1,11 @@
+function Funcionalidades() {
+    return (
+        <div className="container">
+        <h1>Funcionalidades</h1>
+        <p>Esta página lista as funcionalidades do sistema.</p>
+        <p>Em breve, mais informações serão adicionadas.</p>
+        </div>
+    );
+}
+
+export default Funcionalidades;
